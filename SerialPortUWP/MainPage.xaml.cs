60cc0eb971fd9a410291e4ce2c53f1a1a26c3234
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-30 20:58:37 -0700

    Added appropriate includes

--- SerialPortUWP/MainPage.xaml.cs
+++ SerialPortUWP/MainPage.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
