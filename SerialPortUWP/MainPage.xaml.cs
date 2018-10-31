3ece107fe6175058822c8ff0901f4077c129317a
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-30 20:52:49 -0700

    Made first stackpanel

--- SerialPortUWP/MainPage.xaml.cs
+++ SerialPortUWP/MainPage.xaml.cs
@@ -26,5 +26,9 @@ namespace SerialPortUWP
         {
             this.InitializeComponent();
         }
+
+        private void btnConnectToDevice_Click(object sender, RoutedEventArgs e) {
+
+        }
     }
 }
